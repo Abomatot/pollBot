0c4ec070ba4e149ecc4584d43e27add665d9021a
Abomatot <jeff_3@gmx.de>
2021-12-11 23:32:28 +0100

    first commit

---
+++ Polly.py
@@ -0,0 +1,84 @@
+import discord as discord
+from belissibot_framework import App
+
+message_of_user_id_dict = {}
+app = App()
+
+
+@app.route("!poll", raw_args=True)
+async def poll(client: discord.Client, message: discord.Message):
+    help_embed = discord.Embed(title="Usage of `!poll`", colour=discord.Color(0x7100aa),
+                          description="Tip: Add a `help` to any command to show its help.")
+    help_embed.add_field(name="`!poll new`", value="Creates a new poll.", inline=False)
+    help_embed.add_field(name="`!poll add <answer>`", value="Adds a option two answer to your poll.", inline=False)
+    help_embed.add_field(name="`!poll remove <number>`", value="Removes the specified option to answer from your poll.", inline=False)
+    help_embed.add_field(name="`!poll publish`", value="Makes the poll uneditable and sets up the reactions.", inline=False)
+
+
+
+
+@app.route("!poll new", raw_args=True)
+async def poll(client: discord.Client, message: discord.Message, question):
+    embed = discord.Embed(title="Frage: " + question, colour=discord.Color(0x7100aa),
+                          description=description_factory([]))
+    embed.set_footer(text="__________________________________________________\n"
+                          "-> to remove a add type !poll remove <number>.\n"
+                          "-> to publish the poll type !poll publish.")
+    message_of_user_id_dict[message.author.id] = (await message.channel.send(embed=embed)).id, []
+    await message.delete()
+
+@app.route("!poll add", raw_args=True)
+async def poll(client: discord.Client, message: discord.Message, choice):
+    pollmessage_id, choices = message_of_user_id_dict[message.author.id]
+    pollmessage = await message.channel.fetch_message(pollmessage_id)
+    if len(choices) >= 9:
+        return
+    choices.append(choice)
+    embed = pollmessage.embeds[0]
+    embed.description = description_factory(choices)
+    await pollmessage.edit(embed=embed)
+    await message.delete()
+
+
+@app.route("!poll remove", raw_args=True)
+async def poll(client: discord.Client, message: discord.Message, number):
+    pollmessage_id, choices = message_of_user_id_dict[message.author.id]
+    pollmessage = await message.channel.fetch_message(pollmessage_id)
+    del choices[int(number) - 1]
+    embed = pollmessage.embeds[0]
+    embed.description = description_factory(choices)
+    await pollmessage.edit(embed=embed)
+    await message.delete()
+
+
+@app.route("!poll publish")
+async def poll(client: discord.Client, message: discord.Message):
+    pollmessage_id, choices = message_of_user_id_dict[message.author.id]
+    pollmessage = await message.channel.fetch_message(pollmessage_id)
+    # react with choicenumbers
+    for index in range(len(choices)):
+        await pollmessage.add_reaction(emoji_factory(index + 1))
+    # delete relation from message of user id
+    del message_of_user_id_dict[message.author.id]
+    await message.delete()
+
+
+def description_factory(choices: list[str]):
+    out = ""
+    for index, line in enumerate(choices):
+        out += "\n" + emoji_factory(index + 1) + f"`{line}`"
+    if len(choices) >= 9:
+        out += "\n\n_maximum number of answers reached_"
+    else:
+        out += "\n\n_add a new option to answer by typing !poll add <your answer>_"
+    return out
+
+
+def emoji_factory(number):
+    return str(number) + b"\xef\xb8\x8f\xe2\x83\xa3".decode()
+
+
+with open("secret.token", "r") as f:
+    TOKEN = f.read()
+
+app.run(discord_token=TOKEN)
